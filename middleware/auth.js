c70e4d8161fde12f4f065a6d6dcf1fbe6c825827
user.name : adedotunbashorun <user.email : adedotunolawale@gmail.com>
2019-07-18 14:22:50 +0100

    worked on middleware

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,5 +1,5 @@
 export default function ({ store, redirect }) {
-    if ((store.state.auth.token == '' || typeof store.state.auth.token == 'undefined')) {
+    if (store.state.auth.token == '' || typeof store.state.auth.token == 'undefined' || store.state.auth.user.user_type != 'admin') {
         return redirect('/login')
     }
 }
