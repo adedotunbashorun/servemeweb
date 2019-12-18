753fb69bf0e66cb3b2fdc8c3cb5589e8dd70d6d2
adedotunbashorun <adedotunolawale@gmail.com>
2019-12-18 16:15:08 +0100

    minor fix on subcat

--- api/index.js
+++ api/index.js
@@ -1,5 +1,6 @@
 import { User } from './User'
 import { Category } from './Category'
+import { Question } from './Question'
 import { Class } from './Class'
 import { Order } from './Order'
 import { Settings } from './Settings'
@@ -8,6 +9,7 @@ import { Supports } from './Supports'
 export const Api =  {
     User,
     Category,
+    Question,
     Class,
     Settings,
     Supports,
